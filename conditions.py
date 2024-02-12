0bd5141085cd28a2dab51661ecf2ba1943b9d2c4
Wangari-j <wangarijudy@gmail.com>
2024-02-12 23:40:05 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,15 @@
+a = 20
+b = 30
+
+if a<b:
+    print("a is less than b")
+else:
+    print("Incomplete")
+
+g = 200
+h = 500
+
+if g > h:
+    Print("g is greater than h")
+else:
+    print("Not true")
